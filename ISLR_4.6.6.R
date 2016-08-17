cca22d73d736e424e6a90712dd9b5bc0166e0e58
apapiu <alex.papiu@gmail.com>
2016-08-17 00:01:25 -0500

    modified caravan

--- ISLR_4.6.6.R
+++ ISLR_4.6.6.R
@@ -9,67 +9,28 @@ library(ROCR)
 
 data(Caravan)
 
-X <- as.matrix(Caravan[,-86])
-y <- Caravan$Purchase
+X = model.matrix(Purchase ~ ., data = Caravan)
+y = Caravan$Purchase
 
 Caravan %>% count(Purchase)
 #only 6% percent of the people buy insurance.
-set.seed(213)
-sam <- sample(1:dim(Caravan)[1], dim(Caravan)[1]*0.8)
-train <- Caravan[sam,] 
-test <- Caravan[-sam,]
-X_train <- X[sam,]
-X_test <- X[-sam,]
-y_train <- y[sam]
-y_test <- y[-sam]
 
-#~~~~~~~~~~~~~~~~~~~~
-#logistic regression:
-#~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~
+#reg logistic regression:
+#~~~~~~~~~~~~~~~~~~~~~~~~
 
-model_lasso <- cv.glmnet(X_train, y_train, nfolds = 10,
-                         family = "binomial")
+model_lasso <- cv.glmnet(X, y, nfolds = 3,
+                         family = "binomial", type.measure = "auc")
 
 plot(model_lasso)
-#interestingly using all the features gives significantly higher Binomial Deviance
-
-model_lasso_2 <- cv.glmnet(X_train, y_train, nfolds = 10,
-                         family = "binomial", type.measure="auc")
-
-plot(model_lasso_2)
-
-max(model_lasso_2$cvm)
-
-predict(model_lasso, X_test,
-        s = model_lasso$lambda.min,
-        type = "response") -> preds
-
-confusionMatrix(1*(preds > 0.18),  as.numeric(y_test) - 1)
-
-pos <- preds[y_test == "Yes"]
-neg <- preds[y_test == "No"]
-
-mean(replicate(10000, sample(pos,1) > sample(neg,1))) #approximating AUC
-
-#computing AUC using the package ROCR
-pred = prediction(preds, y_test)
-roc = performance(pred, "tpr", "fpr")
-plot(roc)
-
-performance(pred, "auc")@y.values
+model_lasso$cvm %>% max
+#0.7394179
 
 
-#how abut logloss?
+X = model.matrix(Purchase ~ (.)^2, data = Caravan) #add interactions.
+model_lasso <- cv.glmnet(X, y, nfolds = 3,
+                         family = "binomial", type.measure = "auc")
 
-preds_df <- cbind(1-preds, preds)
-y_df <- data.frame(y_test)
-
-y_mat <- model.matrix(~ 0 + y_test, y_df)
-
-MultiLogLoss(y_mat, preds_df)
-
-#now let's perturb things a bit:
-preds[preds < 0.01] <- preds[preds < 0.01] + 0.01
 
 #~~~~~~~~~~~~~~~~~~
 #gradient boosting:
@@ -78,15 +39,18 @@ y_xgb <- as.numeric(y) - 1
 y_xgb_test <- y_xgb[-sam]
 y_xgb_train <- y_xgb[sam]
 
-
-nrounds = 25
-xgb.cv(data = X_train, label = y_xgb_train,
-       nrounds = nrounds, objective = "binary:logistic",
-       nfold = 5,
+xgb.cv(data = X, label = y_xgb,
+       nrounds = 200,
+       #booster = "gblinear",
+       objective = "binary:logistic",
+       nfold = 10,
        stratified = TRUE,
-       eta = 0.3,
-       max_depth = 2,
-       metrics = c("auc", "logloss")) -> cv.error
+       eta = 0.1,
+       alpha = 1,
+       max_depth = 3,
+       metrics = c("auc"),
+       #early.stop.round = 5,
+       maximize = TRUE) -> cv.error
 
 plot(cv.error$test.auc.mean)
 #plot(cv.error$test.logloss.mean)
@@ -94,26 +58,8 @@ plot(cv.error$test.auc.mean)
 max(cv.error$test.auc.mean)
 which.max(cv.error$test.auc.mean)
 
-#0.770 with depth = 2, nrounds = 25
+#gblinear: 0.738
+#gbtree_1: 0.766
+#gbtree_2: 0.772
+#gbtree_3: 0.763
 
-model_xgb <- xgboost(data = X_train, label = y_xgb_train,
-                     nrounds = nrounds, objective = "binary:logistic",
-                     eta = 0.3,
-                     max_depth = 2)
-
-predict(model_xgb, X_test) -> preds_xgb
-
-
-pred = prediction(preds_xgb, y_test)
-roc = performance(pred, "tpr", "fpr")
-plot(roc)
-
-performance(pred, "auc")@y.values #this gives AUC.
-
-
-
-#~~~~
-#knn:
-#~~~~
-
-model_knn <- 
